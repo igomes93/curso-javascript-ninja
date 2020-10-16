9e1224380fc2327b8a089875acfad6be14d77f3f
igomes93 <69442236+igomes93@users.noreply.github.com>
2020-10-16 17:06:19 -0300

    Update challenge-13.js

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -79,18 +79,20 @@ Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
 ficar no mesmo nível que os estados já existentes, não em um array separado.
 */
 // ?
+brasil=brasil.concat(nordeste)
 
 
 /*
 Mostre no console os estados em `newSudeste`.
 */
-console.log( '\nEstados em newSudeste:' );
+console.log( '\nEstados em newSudeste:',newSudeste );
+
 // ?
 
 /*
 Mostre no console os estados do `brasil`.
 */
-console.log( '\nAlguns estados do Brasil:' );
+console.log( '\nAlguns estados do Brasil:',brasil );
 // ?
 
 /*
